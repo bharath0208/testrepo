0d2830739c4ee7e271e4e961923c111b565c9581
Bharath Reddy <bharathreddy@Bharaths-MacBook-Pro.local>
2021-04-23 00:21:52 -0400

    push

--- ml/service/src/test/resources/start-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -x
-export DEVELOPMENT=true
-exec "${java.home}/bin/java" -DUSER="${user.name}" -Dakka.http.service.interface=localhost "${log.file}" "${config.file}" -cp "${runtime_classpath}" ${main.class}
